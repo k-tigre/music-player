8b1994458e4a2bb60b7f3b15dbb2e1b9a8096a59
Tigre <k.gorbats@gmail.com>
2023-01-07 21:27:47 +0300

    Init app project

---
+++ androidApp/src/main/java/by/tigre/music/player/android/MainActivity.kt
@@ -0,0 +1,11 @@
+package by.tigre.music.player.android
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ androidApp/src/main/java/by/tigre/music/player/android/extension/Android.kt
@@ -0,0 +1,21 @@
+package by.tigre.music.player.android.extension
+
+import android.app.Activity
+import android.app.ActivityManager
+import android.content.Context
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+
+private fun Context.getActivityManager(): ActivityManager =
+    getSystemService(Context.ACTIVITY_SERVICE) as ActivityManager
+
+inline fun <reified T : Activity> Context.start(noinline configure: (Intent.() -> Unit)? = null) {
+    startActivity(Intent(this, T::class.java).apply { configure?.invoke(this) })
+}
+
+inline fun <reified T : AppCompatActivity> Activity.startForResult(
+    requestCode: Int,
+    noinline configure: (Intent.() -> Unit)? = null
+) {
+    startActivityForResult(Intent(this, T::class.java).apply { configure?.invoke(this) }, requestCode, null)
+}
