2f81811bd1677aeb0b956d4940f6d10455b7ecaa
Tigre <k.gorbats@gmail.com>
2023-06-03 13:15:59 +0300

    Tech: update dependencies

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,7 +1,6 @@
+@Suppress("UnstableApiUsage")
 dependencyResolutionManagement {
-    @Suppress("UnstableApiUsage")
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
-    @Suppress("UnstableApiUsage")
     repositories {
         google()
         mavenCentral()
