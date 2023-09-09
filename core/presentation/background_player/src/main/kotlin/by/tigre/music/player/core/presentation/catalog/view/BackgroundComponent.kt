ebf79d741047ddd5964e2e728799280a29530724
Tigre <k.gorbats@gmail.com>
2023-09-09 18:16:20 +0300

    Fixed play/pause/next/prev from notification.

--- core/presentation/background_player/src/main/kotlin/by/tigre/music/player/core/presentation/catalog/view/BackgroundComponent.kt
+++ core/presentation/background_player/src/main/kotlin/by/tigre/music/player/core/presentation/catalog/view/BackgroundComponent.kt
@@ -6,24 +6,19 @@ import by.tigre.music.player.core.presentation.backgound_player.di.PlayerBackgro
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.SupervisorJob
-import kotlinx.coroutines.flow.MutableSharedFlow
-import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.flow.distinctUntilChanged
-import kotlinx.coroutines.flow.map
-import kotlinx.coroutines.launch
 import kotlin.coroutines.CoroutineContext
 
 interface BackgroundComponent : CoroutineScope {
 
     val currentSong: StateFlow<Song?>
-    val state: StateFlow<State>
 
     fun getPlayer(): PlaybackPlayer
 
     fun pause()
     fun play()
     fun next()
+    fun prev()
 
     class Impl(dependency: PlayerBackgroundDependency) : BackgroundComponent {
 
@@ -31,58 +26,27 @@ interface BackgroundComponent : CoroutineScope {
         override val coroutineContext: CoroutineContext = Dispatchers.Main + job
 
         private val playbackController = dependency.playbackController
-        private val notificationAction = MutableSharedFlow<Action>()
 
         override val currentSong: StateFlow<Song?> = playbackController.currentItem
-        override val state = MutableStateFlow(State.Paused)
-
-        init {
-            launch {
-//                notificationAction
-//                    .collect { action ->
-//                        when (action) {
-//                            Action.RestoreScreen -> {} // TODO
-//                            Action.Stop -> {
-//
-//                            }
-//
-//                            Action.Play -> TODO()
-//                            Action.Pause -> TODO()
-//                        }
-//                    }
-            }
-
-            launch {
-                playbackController.player.state.map {
-                    if (it == PlaybackPlayer.State.Playing) State.Playing else State.Paused
-                }
-                    .distinctUntilChanged()
-                    .collect(state)
-            }
-        }
 
         override fun getPlayer(): PlaybackPlayer {
             return playbackController.player
         }
 
         override fun pause() {
-//            TODO("Not yet implemented")
+            playbackController.pause()
         }
 
         override fun play() {
-//            TODO("Not yet implemented")
+            playbackController.resume()
         }
 
         override fun next() {
-//            TODO("Not yet implemented")
+            playbackController.playNext()
         }
-    }
 
-    enum class State {
-        Playing, Paused
-    }
-
-    enum class Action {
-        RestoreScreen, Stop, Play, Pause
+        override fun prev() {
+            playbackController.playPrev()
+        }
     }
 }
