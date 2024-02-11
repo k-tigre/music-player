8ddd215d325232c93f1ea3b62cc028e90c29e9cb
Tigre <k.gorbats@gmail.com>
2024-02-11 17:33:36 +0300

    Update app version to 0.12.1

--- buildSrc/src/main/kotlin/Application.kt
+++ buildSrc/src/main/kotlin/Application.kt
@@ -1,6 +1,6 @@
 object Application {
     const val id: String = "by.tigre.music.player"
-    val version: Version = Version(0, 12, 0)
+    val version: Version = Version(0, 12, 1)
     const val name: String = "Music"
 
     const val SDK_COMPILE = 34
