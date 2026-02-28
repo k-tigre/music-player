dae947ca79d247605b9b2478cfc2656e56a5f1f1
Tigre <k.gorbats@gmail.com>
2026-02-28 12:14:49 +0300

    Remove not relevant UI for audiobooks

--- AudioBook/src/main/java/by/tigre/audiobook/presentation/root/component/Root.kt
+++ AudioBook/src/main/java/by/tigre/audiobook/presentation/root/component/Root.kt
@@ -3,19 +3,14 @@ package by.tigre.audiobook.presentation.root.component
 import by.tigre.audiobook.core.presentation.audiobook_catalog.component.RootAudiobookCatalogComponent
 import by.tigre.audiobook.core.presentation.audiobook_catalog.di.AudiobookCatalogComponentProvider
 import by.tigre.music.player.core.presentation.catalog.component.PlayerComponent
-import by.tigre.music.player.core.presentation.catalog.component.RootCatalogComponent
 import by.tigre.music.player.core.presentation.catalog.component.SmallPlayerComponent
-import by.tigre.music.player.core.presentation.catalog.di.CatalogComponentProvider
 import by.tigre.music.player.core.presentation.catalog.di.PlayerComponentProvider
 import by.tigre.music.player.core.presentation.catalog.navigation.PlayerNavigator
-import by.tigre.music.player.core.presentation.playlist.current.component.CurrentQueueComponent
-import by.tigre.music.player.core.presentation.playlist.current.di.CurrentQueueComponentProvider
 import by.tigre.music.player.presentation.base.BaseComponentContext
 import by.tigre.music.player.presentation.base.appChildContext
 import by.tigre.music.player.presentation.base.appChildStack
 import com.arkivanov.decompose.router.stack.ChildStack
 import com.arkivanov.decompose.router.stack.StackNavigation
-import com.arkivanov.decompose.router.stack.bringToFront
 import com.arkivanov.decompose.router.stack.pop
 import com.arkivanov.decompose.router.stack.push
 import com.arkivanov.decompose.value.Value
@@ -27,19 +22,11 @@ import kotlinx.parcelize.Parcelize
 interface Root {
 
     val playerComponent: SmallPlayerComponent
+    val audiobookCatalogComponent: RootAudiobookCatalogComponent
 
     val onStartServiceEvent: Flow<Unit>
 
-    val pages: Value<ChildStack<*, PageComponentChild>>
     val mainComponent: Value<ChildStack<*, MainComponentChild>>
-
-    fun selectPage(index: Int)
-
-    sealed interface PageComponentChild {
-        class Queue(val component: CurrentQueueComponent) : PageComponentChild
-        class Catalog(val component: RootCatalogComponent) : PageComponentChild
-        class AudiobookCatalog(val component: RootAudiobookCatalogComponent) : PageComponentChild
-    }
 
     sealed interface MainComponentChild {
         data object Main : MainComponentChild
@@ -48,13 +35,10 @@ interface Root {
 
     class Impl(
         context: BaseComponentContext,
-        catalogComponentProvider: CatalogComponentProvider,
         playerComponentProvider: PlayerComponentProvider,
-        currentQueueComponent: CurrentQueueComponentProvider,
         audiobookCatalogComponentProvider: AudiobookCatalogComponentProvider,
     ) : Root, BaseComponentContext by context {
 
-        private val pagesNavigation = StackNavigation<PagesConfig>()
         private val mainNavigation = StackNavigation<MainConfig>()
 
         private val playerNavigator = object : PlayerNavigator {
@@ -74,32 +58,13 @@ interface Root {
             )
         }
 
+        override val audiobookCatalogComponent: RootAudiobookCatalogComponent by lazy {
+            audiobookCatalogComponentProvider.createRootAudiobookCatalogComponent(
+                appChildContext("audiobook_catalog")
+            )
+        }
+
         override val onStartServiceEvent = MutableSharedFlow<Unit>()
-
-        override val pages: Value<ChildStack<*, PageComponentChild>> =
-            appChildStack(
-                source = pagesNavigation,
-                initialStack = { listOf(PagesConfig.Queue) },
-                key = "pages",
-                handleBackButton = false
-            ) { config, componentContext ->
-                when (config) {
-                    PagesConfig.Catalog -> PageComponentChild.Catalog(
-                        catalogComponentProvider.createRootCatalogComponent(componentContext)
-                    )
-
-                    PagesConfig.Queue -> PageComponentChild.Queue(
-                        currentQueueComponent.createCurrentQueueComponent(
-                            componentContext,
-                            navigator = { selectPage(1) }
-                        )
-                    )
-
-                    PagesConfig.AudiobookCatalog -> PageComponentChild.AudiobookCatalog(
-                        audiobookCatalogComponentProvider.createRootAudiobookCatalogComponent(componentContext)
-                    )
-                }
-            }
 
         override val mainComponent: Value<ChildStack<*, MainComponentChild>> =
             appChildStack(
@@ -120,25 +85,6 @@ interface Root {
                 }
             }
 
-        override fun selectPage(index: Int) {
-            when (index) {
-                0 -> pagesNavigation.bringToFront(PagesConfig.Queue)
-                1 -> pagesNavigation.bringToFront(PagesConfig.Catalog)
-                2 -> pagesNavigation.bringToFront(PagesConfig.AudiobookCatalog)
-            }
-        }
-
-        private sealed interface PagesConfig : Parcelable {
-            @Parcelize
-            data object Queue : PagesConfig
-
-            @Parcelize
-            data object Catalog : PagesConfig
-
-            @Parcelize
-            data object AudiobookCatalog : PagesConfig
-        }
-
         private sealed interface MainConfig : Parcelable {
             @Parcelize
             data object Main : MainConfig
@@ -146,8 +92,5 @@ interface Root {
             @Parcelize
             data object Player : MainConfig
         }
-
-        @Parcelize
-        private object Player : Parcelable
     }
 }
