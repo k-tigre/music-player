ee15b7d543607c7b7a47de9187399492c90b6a1d
Tigre <k.gorbats@gmail.com>
2026-03-08 17:24:00 +0300

    Cleanup unused module

--- core/entity/queue/build.gradle.kts
+++
@@ -1,9 +0,0 @@
-plugins {
-    id(Plugin.Id.AndroidLibrary.value)
-    id(Plugin.Id.KotlinAndroid.value)
-}
-
-dependencies {
-    implementation(Library.KotlinStd)
-    implementation(Project.Core.Music.Entity.Catalog)
-}
